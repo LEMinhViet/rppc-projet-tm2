aa9de6b4b723fe6c975c037274208be6d9efa06b
lan.612zhou@gmail.com <lan.612zhou@gmail.com@295e3ff4-03c6-352a-5290-9869b4b737e2>
2014-01-12 15:07:03 +0000

    Affichage de resultat

--- trunk/RPPC_Projet_2/src/rppc/Greedy.java
+++ trunk/RPPC_Projet_2/src/rppc/Greedy.java
@@ -10,59 +10,62 @@ public class Greedy {
 	private int[] capacite;
 
 	private int nombreDeTache;
-	    
+
 	/**
 	 * Initialisation
-	 * @param taches : les taches de probleme 
-	 * @param capacite : les capacite des agents
+	 * 
+	 * @param taches
+	 *            : les taches de probleme
+	 * @param capacite
+	 *            : les capacite des agents
 	 */
 	public Greedy(List<Tache> taches, int[] capacite) {
 		this.taches = taches;
 		this.capacite = capacite;
-		
+
 		nombreDeTache = taches.size();
 		affectation = new int[nombreDeTache];
 		for (int i = 0; i < nombreDeTache; i++) {
 			affectation[i] = -1;
 		}
 	}
-	
+
 	/**
-	 * L'algorithme glouton : 
-	 * Pour chaque tache 
-	 * - Trier les agents - trouver les meilleur (coût plus petit)
-	 * 	 + Si ne pas pouvoir trouver un agent => echouer 
-	 * - Assigner la tache a l'agents, diminuer sa capacite
+	 * L'algorithme glouton : Pour chaque tache - Trier les agents - trouver les
+	 * meilleur (coï¿½t plus petit) + Si ne pas pouvoir trouver un agent =>
+	 * echouer - Assigner la tache a l'agents, diminuer sa capacite
 	 */
 	public void traiter() {
 		for (int i = 0; i < nombreDeTache; i++) {
-    		taches.get(i).trierCout();
-    		taches.get(i).calculerCandidate(capacite);
-    		
-    		if (taches.get(i).getAgentCandidate() == -1) {
-    			System.out.println("NO RESULT in step " + i);
-    			continue;
-    		} else {
-    			affectation[i] = taches.get(i).getAgentCandidate();
-    			capacite[affectation[i]] -= taches.get(i).getRessource(affectation[i]);
-    		}
-    	}
-    	
-		afficherResultat();    	
-    }
-	
+			taches.get(i).trierCout();
+			taches.get(i).calculerCandidate(capacite);
+
+			if (taches.get(i).getAgentCandidate() == -1) {
+				System.out.println("NO RESULT in step " + i);
+				continue;
+			} else {
+				affectation[i] = taches.get(i).getAgentCandidate();
+				capacite[affectation[i]] -= taches.get(i).getRessource(affectation[i]);
+			}
+		}
+
+		afficherResultat();
+	}
+
 	public int[] getAffectation() {
-    	return affectation;
-    }
-	
+		return affectation;
+	}
+
 	private void afficherResultat() {
 		int cout = 0;
-    	for (int i = 0; i < nombreDeTache; i++) {
-//    		System.out.println("tache : " + i + " with agent " + affectation[i]);
-    		cout += affectation[i] != -1 ? taches.get(i).getCout(affectation[i]) : 0;
-    	}
-    	System.out.println("Algorithme glouton - Cout total : " + cout);
+		for (int i = 0; i < nombreDeTache; i++) {
+			// System.out.println("tache : " + i + " with agent " +
+			// affectation[i]);
+			cout += affectation[i] != -1 ? taches.get(i).getCout(affectation[i]) : 0;
+		}
+		for (int i = 0; i < nombreDeTache; i++)
+			System.out.println("Tache " + i + " est effectuee par l'agent " + affectation[i]);
+		System.out.println("Algorithme glouton - Cout total : " + cout);
 	}
-	
-	
+
 }
